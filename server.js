30ba49681cc7c61fbc026b8b4dbd07954dab4d66
SUMAN KUMAR <86798449+imSuMN@users.noreply.github.com>
2023-01-09 12:45:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,19 +24,11 @@ app.use("/api/message", messageRoutes);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
-}
+
 
 // --------------------------deployment------------------------------
 
@@ -52,7 +44,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 120000,
   cors: {
-    origin: "https://weeechat-sk.herokuapp.com/",
+    origin: "https://vercel.com/imsumn/weeechat-sk";
     // credentials: true,
   },
 });
